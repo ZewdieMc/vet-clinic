8b674d9009616c998373259ec909fe695c5c63bb
Zewdie Habtie <zewdiehabtie26@gmail.com>
2023-05-08 21:12:39 +0000

    Create savepoint

--- queries.sql
+++ queries.sql
@@ -23,3 +23,53 @@ SELECT * FROM animals WHERE name !='Gabumon';
 
 --Find all animals with a weight between 10.4kg and 17.3kg
 SELECT * FROM animals WHERE weight_kg BETWEEN 10.4 AND 17.3;
+
+-- begin transaction
+BEGIN;
+--pdate the animals table by setting the species column to unspecified
+UPDATE animals SET species='unspecified';
+--RoLLback the transaction
+ROLLBACK;
+
+--BEGIN TRANSACTION
+BEGIN;
+--Update the animals table by setting the species column to digimon for all animals that have a name ending in mon.
+UPDATE animals SET species='digimon' WHERE name LIKE '%mon';
+
+--Update the animals table by setting the species column to pokemon for all animals that don't have species already set.
+UPDATE animals SET species='pokemon' WHERE species IS NULL;
+
+--VERIFY CHANGES WERE MADE
+SELECT * FROM animals;
+
+--COMMIT TRANSACTION
+COMMIT;
+
+--Verify that changes persist after commit.
+SELECT * FROM animals;
+
+-- DELETE ALL RECORDS IN THE ANIMALS TABLE
+DELETE FROM animals;
+
+--ROLLBACK
+ROLLBACK;
+
+--BEGIN TRANSACTION
+BEGIN;
+--Delete all animals born after Jan 1st, 2022.
+DELETE FROM animals WHERE date_of_birth > '2022-01-01';
+
+--Create a savepoint
+SAVEPOINT delete_young;
+
+--Update all animals' weight to be their weight multiplied by -1.
+UPDATE animals SET weight_kg = weight_kg*-1;
+
+--ROLLBACK TO THE SAVEPOINT
+ROLLBACK TO delete_young;
+
+--Update all animals' weights that are negative to be their weight multiplied by -1.
+UPDATE animals SET weight_kg = weight_kg*-1 WHERE weight_kg < 0;
+
+--COMMIT TRANSACTION
+COMMIT;
