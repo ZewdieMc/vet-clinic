62a950246ae52dc6ee15de59b77824a72253fbad
Zewdie Habtie <zewdiehabtie26@gmail.com>
2023-05-08 13:36:17 +0000

    Create animals table

--- schema.sql
+++ schema.sql
@@ -1,6 +1,12 @@
 /* Database schema to keep the structure of entire database. */
 
---CREATE TABLE
-CREATE TABLE animals(
-    
+--CREATE animals TABLE
+CREATE TABLE animals (
+    id INT GENERATED ALWAYS AS IDENTITY,
+    name TEXT,
+    date_of_birth DATE,
+    escape_attempts INT,
+    neutered BOOLEAN,
+    weight_kg DECIMAL(4, 2),
+    PRIMARY KEY(id)
 );
